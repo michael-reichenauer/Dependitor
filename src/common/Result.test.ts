eebf46dabfd3e0f145946879bd8af082c6e93d88
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-05-26 10:53:01 +0200

    Fix some more errors

--- src/common/Result.test.ts
+++ src/common/Result.test.ts
@@ -30,7 +30,8 @@ describe("Test Result<T> type using isError(value) type guard for narrowing", ()
     const value = getValue("error");
     if (isError(value)) {
       // Value is an Error type (i.e. message exists), verify and exit function
-      expect(value.message).toEqual("invalid");
+      const error = value as Error;
+      expect(error.message).toEqual("invalid");
       return;
     }
 
@@ -76,7 +77,8 @@ describe("Test Result<T> type using instanceof operator(value) for narrowing", (
     const value = getValue("error");
     if (value instanceof Error) {
       // Value is an Error type (i.e. message exists), verify and exit function
-      expect(value.message).toEqual("invalid");
+      const error = value as Error;
+      expect(error.message).toEqual("invalid");
       return;
     }
 
