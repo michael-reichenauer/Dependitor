f336f3d9530868cfc67be597931e04226a1b6a96
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-05-26 10:26:46 +0200

    Fix errors after updating react-scripts

--- src/common/Result.test.ts
+++ src/common/Result.test.ts
@@ -15,7 +15,7 @@ describe("Test Result<T> type using isError(value) type guard for narrowing", ()
     const value = getValue("ok");
     if (isError(value)) {
       // Since value is ok, this path will not occur, but value would be Error type if it did
-      const errorValue: Error = value;
+      const errorValue: Error = value as Error;
       expect(errorValue.message).toBeFalsy();
       assert.fail();
       return;
