c2a220603f431ed47c3cd302a3f01929bd542695
Michael Reichenauer <michael-reichenauer@users.noreply.github.com>
2022-06-17 05:33:51 +0000

    Initial prototype WebAuthn working

---
+++ api/VerifyWebAuthnRegistration/index.js
@@ -0,0 +1,14 @@
+var store = require('../shared/Store.js');
+
+module.exports = async function (context, req) {
+    try {
+        store.verifyApiKey(context)
+        const options = await store.verifyWebAuthnRegistration(context, req.body)
+
+        context.res = { status: 200, body: options };
+    } catch (err) {
+        context.log.error('error:', err);
+        context.res = { status: 400, body: `error: '${err.message}'` };
+
+    }
+}
