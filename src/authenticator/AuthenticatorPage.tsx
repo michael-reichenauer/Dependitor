1f8f7b2a5d9c16746c85ed364be8dfc53ca8de01
Michael Reichenauer <michael-reichenauer@users.noreply.github.com>
2022-07-08 05:30:29 +0000

    Clean

--- src/authenticator/AuthenticatorPage.tsx
+++ src/authenticator/AuthenticatorPage.tsx
@@ -1,8 +1,6 @@
+import React, { useEffect, FC } from "react";
+import { Typography } from "@material-ui/core";
 import Progress from "../common/Progress";
-import { LinearProgress, Typography } from "@material-ui/core";
-import { Form, Formik } from "formik";
-import React, { useEffect } from "react";
-import { FC } from "react";
 import { di } from "../common/di";
 import { IAuthenticatorKey } from "./Authenticator";
 
@@ -13,106 +11,18 @@ export const AuthenticatorPage: FC = () => {
     authenticator.activate();
   });
 
-  const handleEnter = (event: any): void => {
-    if (event.code === "Enter") {
-      const okButton = document.getElementById("OKButton");
-      okButton?.click();
-    }
-  };
-
   return (
     <>
-      <Progress />
-      <Formik
-        initialValues={{ deviceName: "" }}
-        validate={async (values) => {
-          const errors: any = {};
-          // if (!values.username) {
-          //   errors.username = "Required";
-          // }
-          return errors;
-        }}
-        onSubmit={async (values, { setErrors, setFieldValue }) => {
-          // if (createAccount) {
-          //   const createResult = await login?.createAccount({
-          //     username: "",
-          //     password: "",
-          //   });
-          //   if (isError(createResult)) {
-          //     // setFieldValue("password", "", false);
-          //     // setErrors({ username: "User already exist" });
-          //     return;
-          //   }
-          //   // setDefaultUserName(values.username);
-          //   // setCreateAccount(false);
-          //   // setFieldValue("confirm", "", false);
-          // }
-          //   const loginResult = await login?.login();
-          //   if (isError(loginResult)) {
-          //     // setFieldValue("password", "", false);
-          //     // if (isError(loginResult, AuthenticateError)) {
-          //     //   setErrors({ username: "Invalid username or password" });
-          //     // } else {
-          //     //   setErrors({ username: "Failed to enable device sync" });
-          //     // }
-          //     return;
-          //   }
-          //   //setDefaultUserName(values.username);
-          //   setLogin(null);
+      <div
+        style={{
+          margin: 15,
         }}
       >
-        {({ submitForm, isSubmitting }) => (
-          <Form onKeyUp={handleEnter}>
-            {isSubmitting && <LinearProgress style={{ marginBottom: 5 }} />}
-            {/* <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Button
-                    id="LoginButton"
-                    variant="contained"
-                    color="primary"
-                    disabled={isSubmitting}
-                    onClick={submitForm}
-                    style={{
-                      marginTop: 15,
-                      marginBottom: 30,
-                    }}
-                  >
-                    Login
-                  </Button>
-                </div> */}
-
-            <Typography>Your current synced devices:</Typography>
-
-            {/* <div
-              style={{
-                position: "absolute",
-                bottom: 15,
-                width: "100%",
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <Button
-                variant="contained"
-                color="primary"
-                disabled={isSubmitting}
-                onClick={() => {
-                  //   setLogin(null);
-                  //   login?.cancelLogin();
-                }}
-                style={{ width: 85 }}
-              >
-                Cancel
-              </Button>
-            </div> */}
-          </Form>
-        )}
-      </Formik>
+        <Progress />
+        <Typography variant="h6">Devices:</Typography>
+        <Typography>Some device 1</Typography>
+        <Typography>Some device 2 ...</Typography>
+      </div>
     </>
   );
 };
