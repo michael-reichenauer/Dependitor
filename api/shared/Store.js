db9f6a2019430855872f608677ea2a42a129fc5a
Michael Reichenauer <michael-reichenauer@users.noreply.github.com>
2022-07-15 08:58:48 +0000

    Try other random id on server

--- api/shared/Store.js
+++ api/shared/Store.js
@@ -618,9 +618,16 @@ function delay(time) {
     });
 }
 
+
 function makeRandomId() {
-    return randomString(12)
+    let ID = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (var i = 0; i < 12; i++) {
+        ID += characters.charAt(Math.floor(Math.random() * 36));
+    }
+    return ID;
 }
+
 
 function randomString(count) {
     let randomText = "";
