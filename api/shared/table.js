3d5bc089d27b5ef405b91fea65815ff8f25287c3
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-12-31 06:28:03 +0000

    Add some example code

--- api/shared/table.js
+++ api/shared/table.js
@@ -5,6 +5,29 @@ const tableService = azure.createTableService();
 
 const entGen = azure.TableUtilities.entityGenerator;
 
+// /**
+//  * The default credential will use the user-assigned managed identity with the specified client ID.
+//  */
+// function withDefaultAzureCredential() {
+//     // Alternatively, you may set the environment variable AZURE_CLIENT_ID="<MANAGED_IDENTITY_CLIENT_ID>" and omit the `managedIdentityClientId`
+//     // option when using `DefaultAzureCredential` - the two approaches are equivalent.
+//     const credential = new DefaultAzureCredential({
+//       managedIdentityClientId: "<MANAGED_IDENTITY_CLIENT_ID>"
+//     });
+//     const client = new SecretClient("https://key-vault-name.vault.azure.net", credential);
+//   }
+
+// // With table URL and DefaultAzureCredential
+// var client = new Client(
+//     new Uri("https://127.0.0.1:10002/devstoreaccount1/table-name"), new DefaultAzureCredential()
+//   );
+
+// // With connection string
+// var client = new TableClient(
+//     "DefaultEndpointsProtocol=https;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;TableEndpoint=https://127.0.0.1:10002/devstoreaccount1;", "table-name"
+//   );
+
+
 exports.String = (value) => {
     return entGen.String(value)
 }
